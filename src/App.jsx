7e5ab6cb67512df1a638f03989a83f50d4bfee87
jummy16 <ogungbeolajumoke@gmail.com>
2024-08-21 10:57:46 +0100

    correction on App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import Navbar from './components/Navbar/Navbar'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
-import Video from './pages/Video/Video'
+import Video from './pages/video/Video'
 
 const App = () => {
   const [sidebar,setSidebar] = useState(true)
