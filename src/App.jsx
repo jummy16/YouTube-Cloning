d9d5c7734d6862144d1b01b1206ca45d19f23800
jummy16 <ogungbeolajumoke@gmail.com>
2024-08-26 12:32:22 +0100

    coorection on page

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Navbar from './components/Navbar/Navbar'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home/Home'
 import Videos from './pages/Videos/Videos'
-// import Video from './pages/Video/Video'
 
 const App = () => {
   const [sidebar,setSidebar] = useState(true)
@@ -12,7 +11,6 @@ const App = () => {
       <Navbar setSidebar={setSidebar}/>
       <Routes>
         <Route path='/' element={<Home sidebar={sidebar}/>}/>
-        {/* <Route path='/video/:categoryId/:videoId' element={<Video/>} /> */}
         <Route path='/video/:categoryId/:videoId' element={<Videos/>} />
       </Routes>
     </div>
